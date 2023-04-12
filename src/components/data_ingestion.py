bab2dc36d231cd505d92a008be913f73d90fc2fd
Jeet Khamar <jeetkhamar@Jeets-MacBook-Air.local>
2023-04-12 20:27:52 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -45,4 +46,6 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_tranformation = DataTransformation()
-    data_tranformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_tranformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
